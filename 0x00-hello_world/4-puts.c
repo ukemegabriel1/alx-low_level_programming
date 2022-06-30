b62a0ae60fcd822062ffcb481c116365f7a24934
ukemegabriel1 <programmer23456@gmail.com>
2022-06-30 12:52:06 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,21 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- *   * main - Entry point
- *     *
- *       * Return: Always 0 (Success)
- *         */
+ *  * main - Entry point
+ *   *Description:'Write a C program that prints exact
+ *    *Return:Always 0 (Success)
+ *     */
 
 int main(void)
 {
 
 
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
 
-	return (0);
+return (0);
+
+}
+
 
 }
