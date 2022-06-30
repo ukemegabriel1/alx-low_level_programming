8b6108dba67da2e0094557362a148603ea6661f7
ukemegabriel1 <programmer23456@gmail.com>
2022-06-30 08:17:27 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
-
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
+/
+ * main - Entry point
+ *  * Return: Always 0 (sucess)
+ *   */
+ int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}}
