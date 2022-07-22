5cf1ac4e5e5c379ddd4c1ec00b4c76e117e6efa7
ukemegabriel1 <programmer23456@gmail.com>
2022-07-22 10:47:18 -0700

    completed tasks

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,5 @@
 #include "main.h"
-
-/**
- * _puts - prints string
- * @str: input string
- * Return: no return
- */
-
-void _puts(char *str)
+void _puts(char *s)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
+	_putchar('0');
 }
-
